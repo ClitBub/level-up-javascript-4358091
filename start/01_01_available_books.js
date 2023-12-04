7fb64884f4eaae88ffb7570e24b543bfb3407f81
ClitBub <50408845+ClitBub@users.noreply.github.com>
2023-12-04 20:22:28 +0000

    my solution

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,36 @@
 // Write your code here
+class Book {
+  constructor(title, author, ISBN, numCopies)
+  {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  get availability() 
+  {
+    return this.getAvailability();
+  }
+
+  getAvailability()
+  {
+    if (this.numCopies == 0)
+      return "out of stock";
+    else if (this.numCopies <= 10)
+      return "low stock";
+    else
+      return "in stock";
+  }
+
+  sell(numCopiesSold = 1)
+  {
+    this.numCopies -= numCopiesSold;
+  }
+
+  restock(numCopiesStocked = 5)
+  {
+    this.numCopies += numCopiesStocked;
+  }
+}
+
