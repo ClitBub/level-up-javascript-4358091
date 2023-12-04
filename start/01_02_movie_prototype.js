2e9efb4c02a7f811e4f469bf61409d941ecc3744
ClitBub <50408845+ClitBub@users.noreply.github.com>
2023-12-04 20:40:30 +0000

    Made the solution

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,19 @@
 // Write your code here
+function Movie(Title, Director, Genre, Release_year, rating)
+{
+  this.Title = Title;
+  this.Director = Director;
+  this.Genre = Genre;
+  this.Release_year = Release_year;
+  this.rating = rating;
+  
+  this.tellAbout = function()
+  {
+    return this.Title + ", a " + this.Genre + " film directed by " + this.Director + " was released in " 
+    + this.Release_year + ". It received a rating of " + this.rating + ".";
+  }
+}
+
+movie1 = new Movie("Matrix", "Wachowski brothers", "Action", "1999", "98");
+
+console.log(movie1.tellAbout());
